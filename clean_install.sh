73d80e39b74c6129bcdb23e94b071c8373e221cf
amirkb91 <a.bagheri20@imperial.ac.uk>
2023-12-04 15:54:53 +0000

    remove install script

--- clean_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -f /home/akb110/.local/lib/python3.8/site-packages/core.egg-link
-rm -f /home/akb110/.local/lib/python3.8/site-packages/easy-install.pth
-rm -rf core.egg-info
-
-
-pip install --user -e .
