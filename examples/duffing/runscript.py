c41dd92ca472dc8fdf8fe5702b68414b59c07c30
Alan Xavier <alan_xvr@hotmail.com>
2024-02-12 18:11:56 +0000

    Modify to run in another script

--- examples/duffing/runscript.py
+++ examples/duffing/runscript.py
@@ -5,27 +5,28 @@ from core.startingpoint import StartingPoint
 
 from duffing import Duffing
 
-# Problem
-prob = Prob()
-prob.read_contparams("contparameters.json")
-prob.add_doffunction(Duffing.get_fe_data)
-prob.add_icfunction(Duffing.eigen_solve)
-if prob.cont_params["shooting"]["method"] == "single":
-    prob.add_zerofunction(Duffing.time_solve)
-elif prob.cont_params["shooting"]["method"] == "multiple":
-    prob.add_zerofunction(Duffing.time_solve_multiple, Duffing.time_solve)
-    prob.add_partitionfunction(Duffing.partition_singleshooting_solution)
+def run():
+    # Problem
+    prob = Prob()
+    prob.read_contparams("contparameters.json")
+    prob.add_doffunction(Duffing.get_fe_data)
+    prob.add_icfunction(Duffing.eigen_solve)
+    if prob.cont_params["shooting"]["method"] == "single":
+        prob.add_zerofunction(Duffing.time_solve)
+    elif prob.cont_params["shooting"]["method"] == "multiple":
+        prob.add_zerofunction(Duffing.time_solve_multiple, Duffing.time_solve)
+        prob.add_partitionfunction(Duffing.partition_singleshooting_solution)
 
-# Initialise forcing parameters if continuation is forced
-Duffing.forcing_parameters(prob.cont_params)
+    # Initialise forcing parameters if continuation is forced
+    Duffing.forcing_parameters(prob.cont_params)
 
-# Continuation starting point
-start = StartingPoint(prob)
-start.get_startingpoint()
+    # Continuation starting point
+    start = StartingPoint(prob)
+    start.get_startingpoint()
 
-# Logger
-log = Logger(prob)
+    # Logger
+    log = Logger(prob)
 
-# Solve continuation on problem
-con = ConX(prob, start, log)
-con.solve()
+    # Solve continuation on problem
+    con = ConX(prob, start, log)
+    con.solve()
