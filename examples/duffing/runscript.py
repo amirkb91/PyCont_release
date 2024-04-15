3b92229aa1d6518d46c8db9e2174011e595d1457
Alan Xavier <alan_xvr@hotmail.com>
2024-04-15 18:26:07 +0100

    Cleanup

--- examples/duffing/runscript.py
+++ examples/duffing/runscript.py
@@ -35,30 +35,3 @@ def run(model=Duffing, predict_acc=None, pred_energy=None):
     # Solve continuation on problem
     con = ConX(prob, start, log)
     con.solve()
-    
-# def run_LNN(predict_acc, pred_energy):
-#     # Instantiate object with trained LNN
-#     Duffing_LNN.LNN_acceleration(pred_acc=predict_acc)
-#     Duffing_LNN.LNN_energy(pred_energy=pred_energy)
-    
-#     # Problem
-#     prob = Prob()
-#     prob.read_contparams("contparameters.json")
-#     prob.add_doffunction(Duffing_LNN.get_fe_data)
-#     prob.add_icfunction(Duffing_LNN.eigen_solve)
-#     if prob.cont_params["shooting"]["method"] == "single":
-#         prob.add_zerofunction(Duffing_LNN.time_solve)
-
-#     # Initialise forcing parameters if continuation is forced
-#     Duffing_LNN.forcing_parameters(prob.cont_params)
-
-#     # Continuation starting point
-#     start = StartingPoint(prob)
-#     start.get_startingpoint()
-
-#     # Logger
-#     log = Logger(prob)
-
-#     # Solve continuation on problem
-#     con = ConX(prob, start, log)
-#     con.solve()
